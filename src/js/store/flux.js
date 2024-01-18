1875fb39aad6d8d84d1a0cd1ef8bb003c15ec1d6
ingdev8023 <144868641+ingdev8023@users.noreply.github.com>
2024-01-18 06:35:57 +0000

    proyecto al 70% falta el tema del delete

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,80 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			contacts: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			createUser: async () => {
+				try {
+					await fetch("https://playground.4geeks.com/apis/fake/contact/", {
+						method: "POST",
+						body: JSON.stringify([]),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+				} catch (error) {
+					console.error(error);
+				}
+			},
+			obtenerContacts: async () => {
+				try {
+					const openConection = await fetch(
+						"https://playground.4geeks.com/apis/fake/contact/agenda/DJTest4",
+						{
+							method: "GET",
+							headers: {
+								"Content-Type": "application/json"
+							}
+						}
+					);
+					const dataContacts = await openConection.json();
+					setStore({ contacts: dataContacts });
+					console.log(dataContacts);
+				} catch (error) {
+					console.error(error);
+				}
+			},
+			crearContact: async ({ name, email, address, phone }) => {
+				let contact = {
+					full_name: name,
+					email: email,
+					agenda_slug: "DJTest4",
+					address: address,
+					phone: phone
+				};
+				try {
+					const openConection = await fetch("https://playground.4geeks.com/apis/fake/contact/", {
+						method: "POST",
+						body: JSON.stringify(contact),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					const dataContacts = await openConection.json();
+					console.log(dataContacts);
+				} catch (error) {
+					console.error(error);
+				}
+			},
+			deleteContact: async () => {
+				//OBTENER ID
+				try {
+					const openConection = await fetch("https://playground.4geeks.com/apis/fake/contact/", {
+						method: "DELETE",
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					const dataContacts = await openConection.json();
+					console.log(dataContacts);
+				} catch (error) {
+					console.error(error);
+				}
+			}
 		}
 	};
 };
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,29 +1,79 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { Context } from "../store/appContext";
 export const AddContact = () => {
+	const { actions, store } = useContext(Context);
+
+	useEffect(() => {
+		actions.createUser();
+	}, []);
+
+	const [contact, setContact] = useState({ name: "", email: "", phone: 0, address: "" });
+
+	const handleInputChange = e => {
+		const { name, value } = e.target;
+		setContact(contact => ({
+			...contact,
+			[name]: value
+		}));
+		console.log(contact);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		actions.crearContact(contact);
+	};
+
 	return (
 		<div className="container">
 			<div>
 				<h1 className="text-center mt-5">Add a new contact</h1>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div className="form-group">
 						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
+						<input
+							type="text"
+							value={contact.name}
+							onChange={handleInputChange}
+							name="name"
+							className="form-control"
+							placeholder="Full Name"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
+						<input
+							type="email"
+							value={contact.email}
+							onChange={handleInputChange}
+							name="email"
+							className="form-control"
+							placeholder="Enter email"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
+						<input
+							type="phone"
+							value={contact.phone}
+							onChange={handleInputChange}
+							name="phone"
+							className="form-control"
+							placeholder="Enter phone"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
+						<input
+							type="text"
+							value={contact.address}
+							onChange={handleInputChange}
+							name="address"
+							className="form-control"
+							placeholder="Enter address"
+						/>
 					</div>
-					<button type="button" className="btn btn-primary form-control">
+					<button type="submit" className="btn btn-primary form-control">
 						save
 					</button>
 					<Link className="mt-3 w-100 text-center" to="/">
